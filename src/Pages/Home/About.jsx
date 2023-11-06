c68ab2ffebdfef1835f2c9f4498581de39748ac9
03jatin <dahiyajatin162@gmail.com>
2023-11-07 00:01:32 +0530

    first commit

---
+++ src/Pages/Home/About.jsx
@@ -0,0 +1,16 @@
+export default function AboutMe(){
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./images/aboutme.jpeg" alt="" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <h1 className="skills--section--heading">About Me</h1>
+                    <p className="hero--section--description">Hello, I'm Hitika, currently pursuing a BBA in FINTECH. My academic journey has provided me with valuable insights into the dynamic convergence of finance and technology. However, my true passion lies in the world of frontend development. The prospect of creating immersive and user-friendly web interfaces has ignited a desire to transition into coding. This shift represents an exciting new chapter, where I aim to leverage my financial knowledge to craft seamless and visually engaging digital experiences.</p>
+                    <p className="hero--section--description">During my studies, I've had the privilege of gaining practical experience through internships. I honed my communication and community-building skills, fostering engaged and vibrant online communities. Additionally, my time with an esteemed NGO allowed me to contribute to meaningful projects aimed at creating positive social impact. These diverse experiences have enriched my skill set, and I look forward to applying them in my evolving role as a frontend developer." </p>
+                </div>
+            </div>
+        </section>
+    )
+}
